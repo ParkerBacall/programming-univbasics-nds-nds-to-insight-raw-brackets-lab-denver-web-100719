c2e69aacea881cf15fe1e36337f9fa7e7f65ff9d
Parker Bacall <pbacall2@gmail.com>
2019-10-24 02:52:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,7 +15,7 @@ while row_index < nds.length do
   total_gross = 0
   while column_index < nds[row_index].length do
     total_gross += nds[row_index][:movies][column_index][:worldwide_gross]
-    column_index =+ 1
+    column_index += 1
   end
   #add values to hash
   result << {"#{nds[row_index]}" => total_gross}
