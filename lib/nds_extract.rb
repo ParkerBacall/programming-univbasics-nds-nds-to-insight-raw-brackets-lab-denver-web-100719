ef7c29756a82d254534807b942f28cd3ae07ac4a
Parker Bacall <pbacall2@gmail.com>
2019-10-24 03:07:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,10 +12,10 @@ row_index = 0
 
 while row_index < nds.length do
   column_index = 0
-  pp nds[row_index][:name]
+  
   while column_index < nds[row_index].length do
     total_gross += nds[row_index][:movies][column_index][:worldwide_gross]
-    pp total_gross
+
     column_index += 1
   end
   result["#{nds[row_index][:name]}"] = total_gross
