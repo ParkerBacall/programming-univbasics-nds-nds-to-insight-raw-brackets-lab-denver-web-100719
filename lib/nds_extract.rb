5dcce24696061556b0fd08ba60f4ff9256b38d47
Parker Bacall <pbacall2@gmail.com>
2019-10-24 03:06:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,8 +13,8 @@ row_index = 0
 while row_index < nds.length do
   column_index = 0
   pp nds[row_index][:name]
-  while column_index < nds[row_index].length do
-    total_gross += nds[row_index][:movies][column_index][:worldwide_gross]
+  while column_index < nds[row_index][:name].length do
+    total_gross += nds[row_index][:name][:movies][column_index][:worldwide_gross]
     pp total_gross
     column_index += 1
   end
